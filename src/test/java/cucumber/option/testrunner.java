397c737c1919f12c1e2f50e81bff940dd929af89
Himanshu Gilhare <himanshugilhare0666@gmail.com>
2023-08-05 19:15:57 +0530

    himanshu

---
+++ src/test/java/cucumber/option/testrunner.java
@@ -0,0 +1,14 @@
+package cucumber.option;
+
+
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/java/feature",glue={"stepdefination","helper"},tags="@Reg",
+plugin={"html:target/reports/test3.html","json:target/reports/test4.json"})
+public class testrunner {
+
+}
